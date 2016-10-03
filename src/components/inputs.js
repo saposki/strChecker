9c9c9a57ca4f6e0e9c190c88c6ca7e44ac69b275
saposki <saposki@gmail.com>
2016-10-03 04:20:55 -0700

    .

--- src/components/inputs.js
+++ src/components/inputs.js
@@ -7,47 +7,50 @@ class Input extends Component {
     this.state = { term: '' };
   }
 
+render () {
+  return (
+    <div>
+      <div className="col-xs-3">
+        <input
+          value={this.state.term}
+          onChange={eventObject => this.setState({ term: eventObject.target.value })}
+          className="form-control"
+          placeholder="Type or Paste a String" >
+        </input>
+      </div>
+        <input onClick={this.handleClick} type="button" className="btn btn-primary" value="check">
+        </input>
+      <ul>{Object.keys(this.state.term).map((i) => (
+        <ul className="btn btn-info" >
+          <li className="btn btn-success">{i}</li>
+            <br/>
+          <li className="btn btn-success">{this.state.term[i]}</li>
+        </ul>
+      ))}
+      </ul>
+    </div>
+  );
+}
 
-  render () {
-        return (
-        <form>
-          <div className="col-xs-3">
-            <input
-              value={this.state.term}
-              onChange={eventObject => this.setState({ term: eventObject.target.value })} type="text" name="myInput" className="form-control" placeholder="Type of Paste a String" ></input>
-          </div>
-          <input onClick={this.handleClick} type="button" className="btn btn-primary" value="check"></input>
-          <ul>{Object.keys(this.state.term).map((i) => (
-            <ul className="btn btn-info" >
-              <li className="btn btn-success">{i}</li>
-              <br/>
-              <li className="btn btn-success">{this.state.term[i]}</li>
-          </ul>
-        ))}
-          </ul>
-        </form>
-      );
+handleClick = ()=> {
+let string = this.state.term
+
+// function to check analyze string and character frequency
+let unique = (string) =>{
+let obj = {};
+for(let i = 0; i < string.length; i++){
+  if(string.charAt(i) in  obj){
+      obj[string.charAt(i)] = obj[string.charAt(i)] + 1;
+   }else{
+      obj[string.charAt(i)] = 1;
+   }
   }
+  return obj;
+}
+// end of function
 
-  handleClick = ()=> {
-    let string = this.state.term
-
-      // function to check analyze string and character frequency
-    let unique = (string) =>{
-      let obj = {};
-      for(let i = 0; i < string.length; i++){
-        if(string.charAt(i) in  obj){
-            obj[string.charAt(i)] = obj[string.charAt(i)] + 1;
-         }else{
-            obj[string.charAt(i)] = 1;
-         }
-      }
-      return obj;
-    }
-    // end of function
-    this.setState({term: unique(string)});
-  }
-
+this.setState({term: unique(string)});
+}
 }
 
 
